7d81f15fa8397c82be787eb1c9d1f434740cef4d
marc-la <23857377@student.uwa.edu.au>
2025-05-13 00:01:24 +0800

    fixed bogus main to compile

--- phase2/new/src/bogus_main.c
+++ phase2/new/src/bogus_main.c
@@ -8,7 +8,6 @@
 
 #include <stdlib.h>
 #include <unistd.h>
-#include "banned.h"
 
 // bogus implementation of main that links in all account and login
 // functions. You can delete this file and replace it with your own main
@@ -17,20 +16,22 @@ int main(int argc, char *argv[]) {
   (void) argc;
   (void) argv;
 
-  account_t *acc = account_create("", "", "", "");
-  account_free(acc);
-  bool res = account_validate_password(acc, "");
-  res = account_update_password(acc, "");
-  account_record_login_success(acc, 0);
-  account_record_login_failure(acc);
-  res = account_is_banned(acc);
-  res = account_is_expired(acc);
-  account_set_unban_time(acc, 0);
-  account_set_expiration_time(acc, 0);
-  account_set_email(acc, "");
-  (void) account_print_summary(acc, STDOUT_FILENO);
-  (void) res;
-  handle_login("", "", 0, 0, STDOUT_FILENO, STDOUT_FILENO, NULL);
+  // account_t *acc = account_create("", "", "", "");
+  // account_free(acc);
+  // bool res = account_validate_password(acc, "");
+  // res = account_update_password(acc, "");
+  // account_record_login_success(acc, 0);
+  // account_record_login_failure(acc);
+  // res = account_is_banned(acc);
+  // res = account_is_expired(acc);
+  // account_set_unban_time(acc, 0);
+  // account_set_expiration_time(acc, 0);
+  // account_set_email(acc, "");
+  // (void) account_print_summary(acc, STDOUT_FILENO);
+  // (void) res;
+  // handle_login("", "", 0, 0, STDOUT_FILENO, STDOUT_FILENO, NULL);
+  // return 0;
+
   return 0;
 }
 
